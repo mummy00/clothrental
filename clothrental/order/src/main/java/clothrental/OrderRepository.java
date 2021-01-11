fd650c6d3ef0c81f915ea30429d0e1a3443c2e7b
koareum <koareum05@sk.com>
2021-01-11 16:29:40 +0900

    clothrental first commit

---
+++ clothrental/order/src/main/java/clothrental/OrderRepository.java
@@ -0,0 +1,8 @@
+package clothrental;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface OrderRepository extends PagingAndSortingRepository<Order, Long>{
+
+
+}
